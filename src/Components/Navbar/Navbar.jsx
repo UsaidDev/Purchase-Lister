ca9fa654ba50added9ea14e892d7f8c4f724ad00
Usaid <usaidokdev@gmail.com>
2025-10-15 15:06:30 +0530

    Extra changes

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div>
+      <nav class="navbar">
+        <div class="nav-left">
+          <i class="fa-solid fa-cart-shopping"></i>
+          <span class="site-name">Purchase Lister</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
